4b9ce5e3dc45a85c4360239345f9fd5556ff2d58
YerassylDev <erasylbratan2001@gmail.com>
2023-04-08 03:30:59 +0600

    Initial Commit

---
+++ testappforgit/testappforgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappforgitApp.swift
+//  testappforgit
+//
+//  Created by Yerasyl Shyrymbetov on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct testappforgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
